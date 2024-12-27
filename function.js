4a3682bc53ad7a91110261280f70a29ebbf2ba9d
om-gosai <omgosai587@gmail.com>
2024-12-27 22:18:54 +0530

    arrow function

---
+++ function.js
@@ -0,0 +1,16 @@
+// const chai =() => {
+//     let username = "Gosai Om"
+//     console.log(this);
+// }
+
+// chai()
+
+
+// const code = function(sum1,sum2){
+//     return sum1+sum2
+// }
+
+// console.log(code(500,2451))
+
+const test = (sum1,sum2) => sum1 + sum2 ;   
+console.log(test(3524,1524));
